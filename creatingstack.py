878fc47491293a88e05db903f645c8620a904d70
Lucy Browning <lucybrowning@LucysMacBookAir.local>
2020-09-29 15:31:32 +0400

    first draft( not working )

---
+++ creatingstack.py
@@ -0,0 +1,41 @@
+# class stack:
+#     def __init__(self):
+#         self.items = []
+#
+#     def isEmpty(self):
+#         if self.items == 0
+#             print("this is empty")
+#         else:
+#             print ("this is not empty")
+#
+#
+#     def isFull(self):
+#         lengthofitems = len(items)
+#         return lengthofitems
+#         if lengthofitems = self.lengthofitems
+#             print("this stack is full")
+#
+#     def push(self,item):
+#         return self.items.append(items)
+#
+#     def pop(self):
+#         return self.items.pop(items)
+#
+#     def peek(self):
+#         return self.items(list[-1])
+#
+#     def peek(self):
+#         if lengthofitems == 0
+#             print("error this is empty")
+#         else:
+#             print(stack[lengthofitems-1])
+#
+#
+# push = .append
+# pop = .pop
+#
+# return self.items.append(items)
+
+x = ["www.bbc.co.uk"]
+items = items.append(x)
+print(items)
