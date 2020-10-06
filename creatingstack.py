ba4552295f42cad47dd10191079195339462faf1
Lucy Browning <lucybrowning@LucysMacBookAir.local>
2020-10-06 15:37:03 +0400

    using pointers correctly

--- creatingstack.py
+++ creatingstack.py
@@ -6,7 +6,8 @@ class Stack:
             self.size = 0
             print("default used")
 
-        self.stack = []
+        self.stack = [None] * size
+        self.top = -1
         print("your stack has been created")
 
     # def create_list(self):
@@ -15,18 +16,16 @@ class Stack:
 
     def isEmpty(self):
         # len looks @ how many values are in the stack this is 0 if its empty
-        if len(self.stack) == 0:
-            print("stack is empty")
+        if self.top == -1:
             #boolean value is returned
             return True
         else:
-            print ("this is not empty")
             #boolean value is returned
             return False
 
     def isFull(self):
         # checking if the stack is complelty full by looking @ the len
-        if len(self.stack) == len(self.stack):
+        if self.top == self.size:
             print("the stack is full")
             #boolean value is returned
             return True
@@ -37,11 +36,12 @@ class Stack:
 
     def push(self,stack):
         #validates that the item is a string
-        if isinstance(item, str):
+        if isinstance(stack, str):
             if not self.isFull():
+                self.top += 1
+                self.stack[self.top] = stack
                 # if the stack is NOT full then item is added
                 #append adds to the end
-                self.stack.append(item)
                 #sucess is shown to the used
                 print("ITEM HAS BEEN ADDED")
             else:
@@ -59,10 +59,11 @@ class Stack:
             print("STACK EMPTY, INVALID REQUEST")
             #else the top value will be popped
         else:
+            self.top -= 1
+            return self.stack[self.top]
             #the request is valid so it is popped
-            popped = self.stack.pop(items)
             #returns it
-            return popped
+
 
     def peek(self):
         # if the stack is empty there is nothing to peek so its invalid
@@ -70,14 +71,20 @@ class Stack:
             print("STACK EMPTY, INVALID REQUEST")
         else:
             # -1 prints the value at the very top of the stack
-            print(self.stack[-1])
+            print(self.stack[self.top])
 
 
 # push = .append
 # pop = .pop
+#TESTING
+newStack = Stack(15)
+newStack.push("www.dubaicollege.org")
+newStack.push("www.misguided.com")
+newStack.push("www.urban.com")
 
-return self.items.append(items)
-
-x = ["www.bbc.co.uk"]
-items = items.append(x)
-print(items)
+newStack.peek()
+#
+# x = ["www.bbc.co.uk"]
+# y = ["www.dubaicollege.org"]
+# stack = self.stack.append(x)
+# print(stack)
